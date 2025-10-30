80e76e609853fec288233278270a54d2b9a34f1c
Noa González <noa.gonzalez-gomez@epitech.eu>
2025-10-30 11:20:32 +0100

    Revert "[FEATURE] Finalized Holloway NPC, State Management, and UX Polish"
    This reverts commit 1c0f2d40ffda127787ec4605438cccacf91c6743.

--- GAME/ScenarioScript/MaintenanceAreaSS/MaintenanceInterface.gd
+++ GAME/ScenarioScript/MaintenanceAreaSS/MaintenanceInterface.gd
@@ -5,6 +5,7 @@ extends Control
 @onready var enter_npc_container = $EnterNPC 
 @onready var trans1_button = $EnterNPC/TransNPC
 @onready var exit_button = $ExitMaintenance/TransCorridor
+@onready var enter1_sprite = $Enter_1
 @onready var exit_sprite = $Exit
 @onready var background = $MaintenanceBackground
 
@@ -12,6 +13,7 @@ extends Control
 func _ready():
 	if is_instance_valid(GameState) and GameState.holloway_dialogue_finished:
 		enter_npc_container.queue_free()
+		enter1_sprite.queue_free()
 	
 	animator.play("fade_in")
 	
@@ -20,10 +22,23 @@ func _ready():
 	exit_sprite.modulate = Color(1, 1, 1, 0.2)
 
 	if is_instance_valid(trans1_button):
+		# We also check if the sprite is still valid for hover state
+		if is_instance_valid(enter1_sprite):
+			enter1_sprite.modulate = Color(1, 1, 1, 0.2)
+		
 		trans1_button.connect("mouse_entered", Callable(self, "_on_trans_npc_hover_start"))
 		trans1_button.connect("mouse_exited", Callable(self, "_on_trans_npc_hover_end"))
+	
 	exit_button.connect("mouse_entered", Callable(self, "_on_trans_corridor_hover_start"))
 	exit_button.connect("mouse_exited", Callable(self, "_on_trans_corridor_hover_end"))
+
+func _on_trans_npc_hover_start():
+	if is_instance_valid(enter1_sprite):
+		enter1_sprite.modulate = Color(1, 1, 1, 0.4)
+
+func _on_trans_npc_hover_end():
+	if is_instance_valid(enter1_sprite):
+		enter1_sprite.modulate = Color(1, 1, 1, 0.2)
 
 func _on_trans_corridor_hover_start():
 	exit_sprite.modulate = Color(1, 1, 1, 0.4)
